7644995ab27619dc91d8c047433bc32692fdd092
Brendon45 <jejebrendon722@gmail.com>
2024-06-26 23:42:45 +0200

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
-/**
- * Returns the number of arguments passed to the function.
- *
- * @param {...any} args - Any number of arguments.
- * @returns {number} - The number of arguments passed.
- */
 export default function returnHowManyArguments(...args) {
-    return args.length;
-  }
+  return args.length;
+}
