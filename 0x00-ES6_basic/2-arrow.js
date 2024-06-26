1d30e44d1aa4646dcaf88ee2e39b549348285cb3
Brendon45 <jejebrendon722@gmail.com>
2024-06-26 23:31:12 +0200

    Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,14 +1,8 @@
-/**
- * Represents a list of neighborhoods in San Francisco.
- * @constructor
- */
 export default function getNeighborhoodsList() {
-  // Initialize the list of neighborhoods
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  // Add a new neighborhood to the list
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
