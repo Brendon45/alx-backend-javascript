361413d5ab64efc3dbfe2a86850ee916fa3ffd31
Brendon45 <jejebrendon722@gmail.com>
2024-06-27 00:11:57 +0200

    ES6-friendly

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const myarr = [];
-    for (const idx of array) {
-      myarr.push(appendString + idx);
-    }
-  
-    return myarr;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}
