2b45263c2df80401f050325d0081c75832e447f8
Brendon45 <jejebrendon722@gmail.com>
2024-08-20 11:07:15 +0200

    Solution to task 6. Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,3 +1,11 @@
+/*
+  This code sets up a basic HTTP server using Express.js.
+  - The server listens on the specified port (1245).
+  - It defines a single GET route for the root path ('/'), which responds with the text "Hello Holberton School!".
+  - The server is started and listens for incoming connections, logging a message once it's running.
+
+  The server is also exported as a module for potential reuse or testing.
+*/
 const express = require('express');
 
 const PORT = 1245;
